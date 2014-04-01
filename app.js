1d1448f46e1673dad881a570e13c0018c8d1b664
remy <remy@Ruff.(none)>
2014-04-01 15:17:14 +0200

    code tuto implémenté

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var io = require('socket.io').listen(app);
 var express = require('express');
 var stylus = require('stylus');
 var ejs = require('ejs');
@@ -17,10 +18,6 @@ app.get('/', function(req, res) {
     res.render('home.ejs');
 });
 
-app.get('/compter/:nombre', function(req, res) {
-    res.render('page.ejs', {compteur: req.params.nombre});
-});
-
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.send(404, 'Page introuvable !');
@@ -28,4 +25,29 @@ app.use(function(req, res, next){
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// le code ci dessous provient de http://tutorialzine.com/2012/08/nodejs-drawing-game/
+
+// If the URL of the socket server is opened in a browser
+function handler (request, response) {
+
+    request.addListener('end', function () {
+        fileServer.serve(request, response); // this will return the correct file
+    });
+}
+
+// Delete this row if you want to see debug messages
+io.set('log level', 1);
+
+// Listen for incoming connections from clients
+io.sockets.on('connection', function (socket) {
+
+    // Start listening for mouse move events
+    socket.on('mousemove', function (data) {
+
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('moving', data);
+    });
 });
