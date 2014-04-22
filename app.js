1b0d93a7e3018d5f0964b78da6bf34294a9511b5
Remy <remy.maucourt@yahoo.fr>
2014-04-22 13:04:52 +0200

    tentative counter 4

--- app.js
+++ app.js
@@ -43,21 +43,23 @@ function handler (request, response) {
 }
 
 // Delete this row if you want to see debug messages
-io.set('log level', 1);
+// io.set('log level', 1);
 
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
-    connectCounter++;
+    console.log('dans connection');
+    app.locals.connectCounter++;
+
     // Start listening for mouse move events
     socket.on('mousemove', function (data) {
         // This line sends the event (broadcasts it)
         // to everyone except the originating client.
         socket.broadcast.emit('moving', data);
     });
-    socket.broadcast.emit(connectCounter);
+    socket.broadcast.emit(app.locals.connectCounter);
 });
 
 io.sockets.on('disconnect', function (socket) {
-  connectCounter--;
-  socket.broadcast.emit(connectCounter);
+  app.locals.connectCounter--;
+  socket.broadcast.emit(app.locals.connectCounter);
 });
