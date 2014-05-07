147a4f933f315ef060aacbae7e4e7a935734c85b
Remy <remy.maucourt@yahoo.fr>
2014-05-07 19:42:47 +0200

    correction bug déploiement2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ if (process.env.REDISTOGO_URL) {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     var redis = require("redis");
     var client = redis.createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
+    client.auth(rtg.auth.split(":")[1]);
 } else {
   var redis = require('redis');
   var client = redis.createClient();
