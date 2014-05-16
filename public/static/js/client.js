4fceeb82e53f6e2584c84f2f9bd1f05638e5cfdd
Remy <remy.maucourt@yahoo.fr>
2014-05-16 02:43:56 +0200

    correction bug url

--- public/static/js/client.js
+++ public/static/js/client.js
@@ -6,8 +6,8 @@ $(function(){
     };
 
     // var url = app.locals.url;
-    var url = 'http://localhost:3000';
-    // var url = 'http://expressionlibre.herokuapp.com/';
+    // var url = 'http://localhost:3000';
+    var url = 'http://expressionlibre.herokuapp.com/';
 
     var doc = $(document),
         win = $(window),
