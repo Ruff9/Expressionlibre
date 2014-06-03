685551b86e2a319fdd96f838a44d9256b5b0121d
Remy <remy.maucourt@yahoo.fr>
2014-06-03 23:20:01 +0200

    blocage du mouse wheel zoom

--- public/static/js/client.js
+++ public/static/js/client.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
     scrollLeft: $('#windowSetter').offset().left
   }, 2000);
 
+  $(window).keydown(function(event)
+  {
+    if((event.keyCode == 107 && event.ctrlKey == true) || (event.keyCode == 109 && event.ctrlKey == true))
+    {
+      event.preventDefault();
+    }
+
+    $(window).bind('mousewheel DOMMouseScroll', function(event)
+    {
+      if(event.ctrlKey == true)
+      {
+        event.preventDefault();
+      }
+    });
+  });
 });
 
 $(function(){
