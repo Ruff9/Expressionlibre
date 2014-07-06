960623ea957c36892167e8849578f1f1b42eca10
Remy <remy.maucourt@yahoo.fr>
2014-07-06 18:33:24 +0200

    changement texture principale

--- public/static/js/client.js
+++ public/static/js/client.js
@@ -25,20 +25,12 @@ $(function(){
     scrollLeft: $('#windowSetter').offset().left
   }, 2000);
 
-
-  $(window).keydown(function(event)
-  {
+  $(window).keydown(function(event) {
     if((event.keyCode == 107 && event.ctrlKey == true) || (event.keyCode == 109 && event.ctrlKey == true))
-    {
-      event.preventDefault();
-    }
+    { event.preventDefault(); }
 
-    $(window).bind('mousewheel DOMMouseScroll', function(event)
-    {
-      if(event.ctrlKey == true)
-      {
-        event.preventDefault();
-      }
+    $(window).bind('mousewheel DOMMouseScroll', function(event){ 
+      if(event.ctrlKey == true){ event.preventDefault(); }
     });
   });
 
@@ -110,11 +102,10 @@ $(function(){
     });
 
     messages[data.id] = $('<div class="message">'+ data.contenu +'</div>').appendTo('#messages');
-    
+
     messages[data.id].css({
       'left': data.posX + "px",
-      'top': data.posY + "px",
-      'width': '560px'
+      'top': data.posY + "px"
     });
 
   };
