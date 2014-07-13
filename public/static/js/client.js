a103a39056c8ca8991c6bfd44e414df8cac23e9f
Remy <remy.maucourt@yahoo.fr>
2014-07-13 14:52:42 +0200

    modal about refactoring

--- public/static/js/client.js
+++ public/static/js/client.js
@@ -25,48 +25,38 @@ $(function(){
 
   // modal about 
 
-  //Lorsque vous cliquez sur un lien de la classe poplight et que le href commence par #
-  $('a.poplight[href^=#]').click(function() {
-    var popID = $(this).attr('rel'); //Trouver la pop-up correspondante
-    var popURL = $(this).attr('href'); //Retrouver la largeur dans le href
+  $('a.poplight').on('click', function() {
+    console.log(this)
+    var popID = $(this).data('rel');
+    var popWidth = $(this).data('width');
 
-    //Récupérer les variables depuis le lien
-    var query= popURL.split('?');
-    var dim= query[1].split('&amp;');
-    var popWidth = dim[0].split('=')[1]; //La première valeur du lien
-
-    //Faire apparaitre la pop-up et ajouter le bouton de fermeture
-    $('#' + popID).fadeIn().css({
-      'width': Number(popWidth)
-    })
-    .prepend('<a href="#" class="close"><img src="/static/images/close-icon.png" class="btn_close" title="Fermer" alt="Fermer" /></a>');
-
-    //Récupération du margin, qui permettra de centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
+    $('#' + popID).fadeIn().css({ 'width': popWidth}).prepend('<a href="#" class="close"><img src="/static/images/close-icon.png" class="btn_close" title="Close Window" alt="Close" /></a>');
+    
+    //Récupération du margin pour centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
     var popMargTop = ($('#' + popID).height() + 80) / 2;
     var popMargLeft = ($('#' + popID).width() + 80) / 2;
-
-    //On affecte le margin
-    $('#' + popID).css({
+    
+    $('#' + popID).css({ 
       'margin-top' : -popMargTop,
       'margin-left' : -popMargLeft
     });
-
-    //Effet fade-in du fond opaque
-    $('body').append('<div id="fade"></div>'); //Ajout du fond opaque noir
-    //Apparition du fond - .css({'filter' : 'alpha(opacity=80)'}) pour corriger les bogues de IE
+    
+    //Apparition du fond - 'alpha(opacity=80)' pour corriger les bugs de IE
+    $('body').append('<div id="fade"></div>');
     $('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
-
+    
     return false;
   });
-
-  //Fermeture de la pop-up et du fond
-  $('a.close, #fade').live('click', function() { //Au clic sur le bouton ou sur le calque...
+  
+  
+  //Close Popups and Fade Layer
+  $('body').on('click', 'a.close, #fade', function() { //Au clic sur le body...
     $('#fade , .popup_block').fadeOut(function() {
-      $('#fade, a.close').remove();  //...ils disparaissent ensemble
-    });
+      $('#fade, a.close').remove();  
+  }); //...ils disparaissent ensemble
+    
     return false;
   });
-
 
 
   $("#sidebar").click(function(e){
